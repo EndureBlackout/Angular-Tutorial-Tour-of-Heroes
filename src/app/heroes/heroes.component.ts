d2fc8e7ceada3d6827b603a86ad0830a89d2eade
Jake Ellingson <jake.ellingson@ostusa.com>
2020-05-19 10:34:05 -0500

    Initial Code Commit
    Moved all code into the git repo

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(private heroSerivce: HeroService, private messageService: MessageService) { }
+
+  getHeroes(): void {
+    this.heroSerivce.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  }
+
+}
